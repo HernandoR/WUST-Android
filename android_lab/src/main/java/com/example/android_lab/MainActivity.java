a2aef9a84e766a3e58c276014aed56fad375da7a
刘朕 <lzrichard@icloud.com>
2021-04-07 19:03:54 +0800

    done exp1

--- android_lab/src/main/java/com/example/android_lab/MainActivity.java
+++ android_lab/src/main/java/com/example/android_lab/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -51,31 +53,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int id = v.getId();
+        EditText etUser = findViewById(R.id.edit1);
+        EditText etPass = findViewById(R.id.edit2);
+        String inputUserName = etUser.getText().toString();
+        String inputPassword = etPass.getText().toString();
         switch (id) {
             case R.id.btnToastDisp:
-                dispInputByToast();
+                dispInputByToast(inputUserName);
                 break;
             case R.id.btnLogDisp:
+                Log.i("MainActivity_Log", "onClick: Current input usr name：" + inputUserName);
                 break;
             case R.id.btnSaveSp:
+                SaveSp(inputUserName, inputPassword);
                 break;
             case R.id.btnLogin:
-                Intent intent = new Intent(this, PersonalInfo_Activity.class);
-                startActivity(intent);
+                if (loginCheck(inputUserName, inputPassword)) {
+                    Intent intent = new Intent(this, PersonalInfo_Activity.class);
+                    startActivity(intent);
+                }
                 break;
             case R.id.btnCancel:
+                this.finish();
                 break;
             default:
                 break;
         }
     }
 
-    private void dispInputByToast() {
-        EditText etUser = (EditText) findViewById(R.id.edit1);
-        EditText etPass = (EditText) findViewById(R.id.edit2);
+    boolean userCheck(String inputName) {
+//        boolean tag= false;
+        for (String name : personInfos) {
+            if (inputName.equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean passwordCheck(String inputName, String inputPassword) {
+        return inputPassword.equals("1234567");
+    }
+
+    boolean loginCheck(String inputName, String inputPassword) {
+        if (!userCheck(inputName)) {
+            Toast.makeText(this, "用户名不存在", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            if (passwordCheck(inputName, inputPassword)) {
+                return true;
+            } else {
+                Toast.makeText(this, "用户名或密码错误", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+    }
+
+    private void SaveSp(String inputUserName, String inputPassword) {
+        SharedPreferences UsrSp = this.getSharedPreferences("UserInfo", MODE_PRIVATE);
+        if (UsrSp.edit()
+                .putString(inputUserName, inputPassword)
+                .commit()) {
+            Toast.makeText(this, "存储成功", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "存储失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void dispInputByToast(String inputUserName) {
         //请自己根据逻辑填写判断是否合法用户逻辑程序
-        String inputUserName = etUser.getText().toString();
-        String inputPassword = etPass.getText().toString();
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "当前输入的用户名为" + inputUserName, Toast.LENGTH_SHORT).show();
     }
 }
--- android_lab/src/main/java/com/example/android_lab/PersonalInfo_Activity.java
+++ android_lab/src/main/java/com/example/android_lab/PersonalInfo_Activity.java
@@ -2,13 +2,81 @@ package com.example.android_lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.HashSet;
+import java.util.Set;
 
-public class PersonalInfo_Activity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.HashSet;
+
+public class PersonalInfo_Activity extends AppCompatActivity implements View.OnClickListener {
+    EditText etName;
+    Spinner spClass;
+    CheckBox cbHobby1;
+    CheckBox cbHobby2;
+    CheckBox cbHobby3;
+    CheckBox cbHobby4;
+    RadioGroup rgGrade;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info_);
+        etName = findViewById(R.id.et_name);
+        spClass = findViewById(R.id.dropdown_class);
+        cbHobby1 = findViewById(R.id.checkBox);
+        cbHobby2 = findViewById(R.id.checkBox2);
+        cbHobby3 = findViewById(R.id.checkBox3);
+        cbHobby4 = findViewById(R.id.checkBox4);
+        rgGrade = findViewById(R.id.grade_group);
+        btnSubmit = findViewById(R.id.button);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.button:
+                dispInputByToast();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void dispInputByToast() {
+        Toast.makeText(this, "开始检查", Toast.LENGTH_SHORT).show();
+        Set<CheckBox> CheckBoxes = new HashSet<CheckBox>();
+        CheckBoxes.add(cbHobby1);
+        CheckBoxes.add(cbHobby2);
+        CheckBoxes.add(cbHobby3);
+        CheckBoxes.add(cbHobby4);
+        String hobbys = "";
+        for (CheckBox checkBox : CheckBoxes) {
+            if (checkBox.isChecked()) {
+                hobbys += checkBox.getText() + " ";
+            }
+        }
+        RadioButton rb = findViewById(rgGrade.getCheckedRadioButtonId());
+
+        String msg = "姓名：" + etName.getText().toString() + "\n" +
+                "班级：" + spClass.getSelectedItem().toString() + "\n" +
+                "爱好：" + hobbys + "\n" +
+                "等级：" + rb.getText().toString() + "\n";
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
+
